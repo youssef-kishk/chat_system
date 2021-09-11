7b33385226e32826047b1728e0a9fd5cd6ef83cc
Youssef Kishk <ykishk@gmail.com>
2021-09-11 22:20:25 +0200

    add routes

--- chat_system/config/routes.rb
+++ chat_system/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :applications, only: [:index, :show, :create, :destroy, :update] do
+    resources :chats, only: [:index, :show, :create, :destroy] do
+      resources :messages, only: [:index, :show, :create, :destroy, :update]
+    end
+  end
 end
